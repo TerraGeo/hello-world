9cbf63b08c7c7aacf421377db9ee05a215dbd143
Padavan2 <sivanov047@gmail.com>
2016-05-26 16:24:54 +0300

    Updated Code file

--- FileDemo.java
+++ FileDemo.java
@@ -12,24 +12,23 @@ public class FileDemo {
         String filename = "Zgymbo";
         boolean bool = false;
         File folder = null;
-        Random random = new Random();
-        for (int y = 0; y < Integer.parseInt(args[0]); y++) {
+        for (int y = 0; y < parseIt(args[0]); y++) {
             try {
                 folder = new File(args[3].concat(String.valueOf(y + 1)));
-                bool = folder.mkdir();
+                bool = dirCreate(folder);
                 System.out.println(bool);
             } catch (Exception e2) {
                 e2.printStackTrace();
             }
             System.out.println(y + " " + args[3]);
-            for (int i = 0; i < random.nextInt(Integer.parseInt(args[1])); i++) {
+            for (int i = 0; i < randomize(parseIt(args[1])); i++) {
                 File subfolder = new File(folder, homeSubDirectory.concat(String.valueOf(i + 1)));
-                subfolder.mkdir();
-                for (int i3 = 0; i3 < random.nextInt(Integer.parseInt(args[2])) + 1; i3++) {
+                dirCreate(subfolder);
+                for (int i3 = 0; i3 < randomize(parseIt(args[2])) + 1; i3++) {
                     try {
                         FileWriter writer = new FileWriter(new File(subfolder, filename.concat(String.valueOf(i3++)).concat(".txt")));
                         String string = "";
-                        for (int i4 = 65, i5 = 0; i5 < random.nextInt(Integer.parseInt(args[0])) * random.nextInt(Integer.parseInt(args[1])) * Integer.parseInt(args[2]) + 1024; i5++) {
+                        for (int i4 = 65, i5 = 0; i5 < floatingNumb(args[0], args[1], args[2]); i5++) {
                             string = string.concat(String.valueOf((char) i4++));
                         }
                         writer.write(string);
@@ -42,4 +41,18 @@ public class FileDemo {
 
         }
     }
+    public static Integer randomize(Integer a) {
+        Random random1 = new Random();
+        return random1.nextInt(a);
+    }
+    public static Integer parseIt(String str){
+        return Integer.parseInt(str);
+    }
+    public static Integer floatingNumb(String str1, String str2, String str3){
+        return  randomize(parseIt(str1)) * randomize(parseIt(str2)) * parseIt(str3) + 1024;
+    }
+    public  static boolean dirCreate(File file){
+        return file.mkdir();
+    }
 }
+//randomize(parseIt(args[0])) * randomize(parseIt(args[1])) * parseIt(args[2]) + 1024;
