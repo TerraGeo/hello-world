53f0fb5deaa7359279840a4551706d4086a70ea6
Padavan2 <sivanov047@gmail.com>
2016-06-19 18:25:31 +0300

    Jenkins

--- README.md
+++
@@ -1,4 +0,0 @@
-# hello-world
-First repository to honor programming
-
-I don't want to share any info about myself. I am just a humanbeing like everybody else.
--- src/test/java/InstaTest.java
+++ src/test/java/InstaTest.java
@@ -1,3 +1,4 @@
+import database.DBConnector;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.*;
 
 import static java.lang.System.out;
@@ -16,10 +18,9 @@ import static java.lang.System.out;
  * Created by Billy on 6/4/2016.
  */
 public class InstaTest {
-    public Validator linkToImageName;
     public WebDriver webDriver;
     public static final String HOME_PAGE = "https://www.instagram.com";
-    public static String USER_NAME = "kimkardashian";
+    public static String USER_NAME = "alechka.vcher";
     public static ImageSaver imageSaver;
     public static int scrollCount = 300;
 
@@ -44,7 +45,7 @@ public class InstaTest {
         }
         String inner = linkList2.get(0);
         int numbOfPosts = Integer.parseInt(inner.replaceAll(",", ""));
-        for (int i = 0; i < (numbOfPosts + 2) / 12; i++) {
+        for (int i = 0; i < (numbOfPosts / 12) + 1; i++) {
             jse.executeScript(String.format("scrollBy(0, %d)", scrollCount += 250));
             Thread.sleep(1000);
         }
@@ -62,6 +63,35 @@ public class InstaTest {
                 e.printStackTrace();
             }
         }
+    }
+    @Test
+    public void photosToDB() throws SQLException, ClassNotFoundException, InterruptedException {
+        webDriver.get(HOME_PAGE + "/" + USER_NAME);
+        JavascriptExecutor jse = (JavascriptExecutor) webDriver;
+        jse.executeScript("window.scrollBy(0, 1500)", "");
+        webDriver.findElement(By.xpath("//a[@class='_oidfu']")).click();
+        List<WebElement> webElementList1 = webDriver.findElements(By.xpath("//span[@class='_e8fkl']"));
+        List<String> linkList2 = new ArrayList<String>();
+        for (WebElement web2 : webElementList1) {
+            linkList2.add(web2.getText());
+        }
+        String inner = linkList2.get(0);
+        int numbOfPosts = Integer.parseInt(inner.replaceAll(",", ""));
+        for (int i = 0; i < (numbOfPosts / 12) + 1; i++) {
+            jse.executeScript(String.format("scrollBy(0, %d)", scrollCount += 250));
+            Thread.sleep(1000);
+        }
+        List<WebElement> webElementList = webDriver.findElements(By.xpath("//div[@class='_jjzlb']//img"));
+        Set<String> linkSet = new HashSet<String>();
+        for (WebElement web : webElementList) {
+            linkSet.add(Validator.validator(web.getAttribute("src")));
+        }
+        linkSet.stream().forEach(out::println);
+        System.out.println(linkSet.size());
+        linkSet.stream().forEach(url -> DBConnector.getInstance().dbFileWriter(url));
+        /*for (String str : linkSet) {
+            DBConnector.getInstance().dbFileWriter(str);
+        }*/
     }
 
     @After
